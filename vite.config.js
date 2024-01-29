3301e5ab87a5ac13b08f50f68707860c2d930237
chykea <78532156@qq.com>
2024-01-29 14:34:03 +0800

    directory optimize

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSSL from '@vitejs/plugin-basic-ssl'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
 
@@ -9,5 +13,15 @@ export default defineConfig({
     host: '0.0.0.0', // 允许外部访问
     https: true, // 启用https
   },
-  plugins: [vue(), basicSSL()],
+  plugins: [
+    vue(),
+    basicSSL(), // 本地服务开启https的自签名证书
+    // ele 自动导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
 })
